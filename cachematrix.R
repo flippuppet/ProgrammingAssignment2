687de3179ffc8362239e57db6b7b195bdb062935
flippuppet <flippuppet@gmail.com>
2014-07-27 21:20:18 +1200

    fixed up the comments and changed the variable for the matrix to i
    I guess this makes it look more like I've not just copied and pasted
    from the example. And hey it's cool to play around with github etc

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## this function creates a cache to hold the inverse and data
-
+## makeCacheMatrix creates a cache to hold the inverse and data
+## the cache stores the value in the environment of the makeCacheMatrix function for later retrieval
+## the funtions get and set function as then returned as a list capturing the cached values from the outer function scope
 makeCacheMatrix <- function(x = matrix()) {
-		    m <- NULL
+		    i <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                i <<- NULL
         }
         get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-		    getinverse <- function() m
+        setinverse <- function(inverse) i <<- inverse
+		    getinverse <- function() i
         l<-list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
-## this function either gets the caches inverse or calculates the inverse caches it then returns it
-
+## cacheSolve either gets the cache's inverse if it already has it
+## or else calculates the inverse, then caches returning the solution
 cacheSolve <- function(x, ...) {
-		    m <- x$getinverse()
-        if(!is.null(m)) {
+		    i <- x$getinverse()
+        if(!is.null(i)) {
                 message("getting cached data")
-                return(m)
+                return(i)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
 
-
+#rough test code
 #x=rbind(c(1, -1/4), c(-1/4, 1))  
-#solve(x)
+#solve(x) # this result should equal this
 #c<-makeCacheMatrix(x)
 #cacheSolve(c)
+
